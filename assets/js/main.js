2d1d2c0303a07cad61cab0b50ef6b79d15b46105
Fidan Mirizade <fidan.mirizadaa@gmail.com>
2021-09-14 00:03:20 +0400

    add mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,31 @@ langMenuListItems.forEach(item => {
         langMenuList.classList.remove('show-lang-menu');
     });
 });
+
+// mobile menu
+
+const mobileMenuIcon = document.querySelector('.mobile-menu__bar-icon');
+const mobileMenuContainer = document.querySelector(
+    '.header .mobile-menu__container'
+);
+const menuCloseButton = document.querySelector(
+    '.header .mobile-menu__container .close-button'
+);
+
+mobileMenuIcon.addEventListener('click', function() {
+    mobileMenuContainer.classList.add('show-mobile-menu');
+});
+
+menuCloseButton.addEventListener('click', function() {
+    mobileMenuContainer.classList.remove('show-mobile-menu');
+});
+
+// dont work
+
+// document.getElementsByTagName('html').addEventListener('click', function() {
+//     console.log('yes');
+//     if (langMenu.classList.contain('show-lang-menu')) {
+//         console.log('yes');
+//         langMenuList.classList.remove('show-lang-menu');
+//     }
+// });
